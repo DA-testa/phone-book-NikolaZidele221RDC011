c5d29c85ce04452809b0dd8895b7ea9da31491b8
NikolaZidele221RDC011 <Nikola.Zidele@edu.rtu.lv>
2023-04-02 22:27:07 +0300

    .

--- main.py
+++ main.py
@@ -7,40 +7,39 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
 
+
 def write_responses(result):
     print('\n'.join(result))
+
 
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = [None] * 10000000
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            contacts[cur_query.number] = cur_query.name
+
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if contacts[cur_query.number] is not None:
+                contacts[cur_query.number] = None
+
+        elif cur_query.type == 'find':
+            if contacts[cur_query.number] is None:
+                response = 'not found'
+            else:
+                response = contacts[cur_query.number]
+
             result.append(response)
     return result
+
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
